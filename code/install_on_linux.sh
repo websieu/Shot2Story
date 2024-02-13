21b618a7292c08f7bf4c9a1468a3a477755b269c
websieu <websieu@gmail.com>
2024-02-13 18:44:49 +0000

    update config

---
+++ code/install_on_linux.sh
@@ -0,0 +1,31 @@
+git clone https://github.com/websieu/Shot2Story Shot2Story
+cd /root/Shot2Story/code
+conda init bash
+source ~/.bashrc
+apt install libopenmpi-dev gcc
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+conda env update --name shot2story --file conda_env.yml
+source activate shot2story && conda env list
+
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0-py3-none-any.whl
+pip install git+https://github.com/Maluuba/nlg-eval.git@master
+pip install -U openmim
+mim install mmcv
+pip install scenedetect
+pip install huggingface_hub
+
+mkdir /root/Shot2Story/code/pretrain && cd /root/Shot2Story/code/pretrain
+
+wget https://huggingface.co/ByteDance-new/shot2story/resolve/main/shot_av_best_epoch.pth
+wget https://huggingface.co/ByteDance-new/shot2story/resolve/main/transnetv2-pytorch-weights.pth
+
+cd /root/Shot2Story
+
+git add .
+git config --global user.email "raymondt0809@gmail.com"
+git config --global user.name "Raymondt"
+git config pull.rebase true
+git commit -m "a"
+
+cd /root/Shot2Story/code
+python demo_video.py
